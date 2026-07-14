964d3429e8c820cae8855bed5d58ac9dd842d5dd
[dyad] <Pers0naL276276@gmail.com>
2026-07-14 17:42:30 +0300

    [dyad] Обновлён CSS: добавлены плавные hover‑эффекты, тени, трансформации и улучшена читаемость текста. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/landing/MethodCard.tsx
+++ src/components/landing/MethodCard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card";
-import Button from "@/components/ui/button";
+import {Button} from "@/components/ui/button";
 import { Link } from "react-router-dom";
 
 type Method = {
